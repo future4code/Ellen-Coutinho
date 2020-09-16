e0c33a9d14f3f41a5fde37eb9a5503e1b79fb79d
Le Richter <lerichter.c@gmail.com>
2020-09-16 14:15:45 -0300

    exercicios de variaveis

---
+++ quinzena3/variaveis/index.js
@@ -0,0 +1,79 @@
+/*console.log("Hello World")*/
+
+/*
+Resposta1. 
+console.log(b) = 10
+console.log(a, b) = 10, 5 
+
+Resposta2.
+console.log(a, b, c) = 10, 20, 10
+*/
+
+/*
+let name
+let age
+
+console.log(typeof(name))
+console.log(typeof(age))
+
+//d)impressão foi undefined devido não ter uma descrição da variável
+
+name = prompt ("Qual o seu nome?")
+age = prompt ("Qual sua idade?")
+
+console.log(typeof(name))
+console.log(typeof(age))
+
+    //e)no momento da impressão, não foi declarado nenhuma variável, 
+    //após colocar as informações na caixa aberta, o retorno foi como string, 
+    //pois foi informado um texto. 
+
+console.log("Olá,", name, "você tem", age, "anos.") 
+*/
+
+/*Exercicio2.
+let color = prompt ("Qual sua cor preferida?")
+let dog = prompt ("Qual o nome do seu cachorro?")
+let choice = prompt ("Você prefere praia ou campo?")
+let food = prompt ("Qual sua comida preferida?")
+let drink = prompt ("Qual sua bebida preferida?")
+
+console.log("Resposta:" +color)
+con/*sole.log("Resposta:" +dog)
+console.log("Resposta:" +choice)
+console.log("Resposta:" +food)
+console.log("Resposta:" +drink)
+*/
+
+/*Exercício3.
+let foods = ["chocolate", "chocolate branco", "chocolate meio amargo", "chocolate belga", "chocolate suiço"]
+
+console.log(typeof(foods))
+console.log(foods)
+
+console.log("Essas são minhas comidas favoritas:" +foods)
+
+
+foods = prompt ("Qual a sua comida favorita?")
+console.log(foods)
+*/
+
+
+/*Exercício3.c
+let foods = ["chocolate", "chocolate branco", "chocolate meio amargo", "chocolate belga", "chocolate suiço"]
+
+let userChoice = prompt ("Qual a sua comida favorita?")
+
+foods[1] = userChoice
+
+console.log("Essas são minhas comidas favoritas:" +foods)
+*/
+
+/*Exercício4.
+let questions = ["Você ja elogiou um dev hoje?", "Você já sorriu hoje?", "Alguém já disse que você é linda(o) hoje?"]
+let answers = ["true", "true", "false"]
+
+console.log(questions[0], answers[0])
+console.log(questions[1], answers[1])
+console.log(questions[2], answers[2])
+*/
