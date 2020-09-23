fc588446ce39a4e54ad04367b261d16019b867f9
Le Richter <lerichter.c@gmail.com>
2020-09-22 22:19:08 -0300

    pendencia - Operadores

---
+++ quinzena3/operadores/boilerplate/index.js
@@ -0,0 +1,55 @@
+//console.log("Hello World")
+//
+//
+//Exercicio1. 
+//a. false
+//b. fasle
+//c. true
+//e. typeof - boolean
+//
+//Exercicio2.
+//a. undefined
+//b. null
+//c. 11 
+//d. 3
+//e. 11
+//f. 9
+//
+//Exercicios de escrita de código
+//
+//1a.b.c.d.
+//
+//user = Number(prompt("Sua idade:"))
+//friend = Number(prompt("Idade do seu amigo:"))
+//
+//console.log("Sua idade é maior do que a do seu amigo?", user > friend);
+//
+//2a.b.
+//
+//let num = Number(prompt("Insira um numero par"))
+//
+//console.log(num%2)
+//
+//c.d. nos números pares o resultado é sempre zero, e nos números ímpares o resultado sempre sobra 1.
+//
+//let listaDeTarefas = []
+//
+//listaDeTarefas.push(prompt("Passear com o cachorro"))
+//listaDeTarefas.push(prompt("Regar os cactos"))
+//listaDeTarefas.push(prompt("Fazer palavras cruzadas"))
+//console.log(listaDeTarefas);
+//
+//listaDeTarefas.splice(Number(prompt("Digite o índice:")),1)
+//console.log(listaDeTarefas)
+//
+//4.
+//
+//let nomeDoUsuario = []
+//let emailDoUsuário = []
+//
+//nomeDoUsuario.push(prompt("Seu Nome"))
+//emailDoUsuário.push(prompt("Seu Email"))
+//
+//console.log("O e-mail", emailDoUsuario, "foi cadastrado com sucesso. Seja bem-vinda(o),", nomeDoUsuario "!")
+//
+
