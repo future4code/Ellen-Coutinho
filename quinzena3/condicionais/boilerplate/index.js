beca3f0c79c51e11a9aaaf7dbe2d50fe656bc355
Le Richter <lerichter.c@gmail.com>
2020-09-29 19:45:56 -0300

    Aula 11 - Condicionais

---
+++ quinzena3/condicionais/boilerplate/index.js
@@ -0,0 +1,82 @@
+//console.log("Hello World")
+
+//Exercicio.1
+// Usuario deve digitar um número, se esse número for zero ou divisivel por 2 (número par), será impresso "Passou no teste" e se o número for diferente de 2, será impresso "Não passou no teste"
+
+//Exercicio.2
+//a. o código serve para identifcar o preço de acordo com a fruta escolhida. 
+//b. O preço da fruta maçã é R$ 2.25.
+//c. O preço da fruta pêra é R$ 5.
+
+//Exercicio.3
+//a. a primeira linha está declarando a constante onde deve ser digitado o número
+//b. Esse número passou no teste. 
+//c.
+
+//Exercicio.4
+//
+//const age = Number(prompt("Qual a sua idade?"))
+//
+//if(age < 18) {
+//   console.log("Você não pode dirigir!")
+//}
+//if(age >= 18) {
+//   console.log("Você pode dirigir!")
+//}
+
+//Exercicio.5
+//
+//const turno = prompt("Digite M (matutino) ou V (vespertino) ou N (noturno)")
+//
+//if(turno === "M"){
+//    console.log("Bom dia!")
+//}
+//if(turno ==="V"){
+//    console.log("Boa tarde!")
+//}
+//if(turno ==="N"){
+//    console.log("Boa noite!")
+//}
+
+//Exercicio.6
+//
+//const turno = prompt("Digite M (matutino) ou V (vespertino) ou N (noturno)")
+//
+//switch (turno) {
+  //  case "M":
+  //  console.log("Bom dia!")
+  //  break;
+  //  case "V":
+  //  console.log("Boa tarde!")
+  //  break;
+  //  case "N":
+  //  console.log("Boa Noite!")
+  //  break;
+//}
+
+//Exercicio.7
+//
+//const movie = prompt("Qual gênero deseja assistir?")
+//const price = Number("Qual o valor do ingresso?")
+//
+//if(movie === "fantasia"){
+    // console.log("Bom filme!")
+//} else{
+    //  console.log("Escolha outro filme!")
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
